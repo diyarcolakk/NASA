e72f8f674ece4453477d1d4a2fbf8b90e690ba90
diyarcolakk <114312886+diyarcolakk@users.noreply.github.com>
2023-01-24 01:50:03 +0300

    Add files via upload
    sadece remove butonu ve , favorilerin kendini çoklama durumu kaldı

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const containerShow = document.querySelector(".card-container-for-nasa");
 const nesneArr = [];
-
+const tempArr = [];
+let myArr = [];
 async function getNasaApi() {
   const counter = 10;
   const nasaKey = "S765qBh4X5m2oYxN8zQZgtNj6gXqHGEGWbB8ba6r";
@@ -18,6 +19,7 @@ async function getNasaApi() {
       </div>
       <h1 class="p-3 ">${temp[i].title}</h1>
       <a href="#" class="giveAttribute my-1 p-3" id="${i}">Add To Favorites</a>
+      <a href="#" class="deleteAttribute hidden my-1 p-3" id="${i}">Remove</a>
       <div class="text-div mt-3 p-3">${temp[i].explanation}</div>
       <span class="bottom-span mt-4 p-3 font-weight-bold">${temp[i].date}</span>
   </div>`;
@@ -25,36 +27,47 @@ async function getNasaApi() {
     containerShow.insertAdjacentHTML("beforeend", card);
   }
   createNasaCard(nasaApiJson);
+  // Cardlar oluştu
+
   //Favorilere ekliyorum best practice sanırım . ÇOK İYİYİM.
   const addToFavorites = document.querySelectorAll(".giveAttribute");
+  const cardContainer = document.querySelectorAll(".little-container");
   const favoritesBtn = document.querySelector(".show-my-favorites");
   const loadMoreBtn = document.querySelector(".show-more-pictures");
-  const nasaContainer = document.querySelector(".card-container-for-nasa");
-  // const cardContainer = document.querySelector(".little-container");
+  //Add Favorites'e tıklandığında gerçekleşir.
   addToFavorites.forEach((data) => {
     data.addEventListener("click", () => {
       nesneArr.push(nasaApiJson[data.id]);
-      /*İlgili add to favorites'e tıklandığında.ID ye karşılık gelen api verisini
-      bir ArrayNesnesinde tutuyorum. Bunu local storage atıp.
-      Yeni sayfada ekrana vereceğim.Ardından, RemoveFavorites dediğim an ,
-      display="block yapıp" arrayden çıkartacağım.Ve local storage'ı güncelleyeceğim.
-      */
-      localStorage.setItem(
-        String(data.id),
-        JSON.stringify(nasaApiJson[data.id])
-      );
+      localStorage.setItem("nesneArr", JSON.stringify(nesneArr));
     });
   });
+  //Atılan favori itemlerin keylerini Array'de tut. Daha sonrası arrayi getItem'e parametre olarak ver.Böylece her biri için işlemi yapmış olursun.
 
-  // console.log(cardContainer);
-  //Favorites butonu
   favoritesBtn.addEventListener("click", function () {
-    nasaContainer.classList.add("hidden");
-  });
+    cardContainer.forEach((x) => {
+      x.remove();
+    });
 
+    const tempStorage = JSON.parse(localStorage.getItem("nesneArr"));
+    //1-)İf koşulu konulmalı içindeki nesne zaten varsa createNasaCard ona göre çalışmalı
+    createNasaCard(tempStorage);
+    const removeBtn = document.querySelector(".deleteAttribute");
+    const removeBtnAll = document.querySelectorAll(".deleteAttribute");
+    const addToFav2 = document.querySelectorAll(".giveAttribute");
+    addToFav2.forEach((data) => {
+      data.remove();
+    });
+    // removeBtn.classList.remove("hidden");
+    removeBtnAll.forEach((data) => {
+      //2-) RemoveBtn click eventi verilmeli
+      data.classList.remove("hidden");
+    });
+  });
   // Load More Butonu
   loadMoreBtn.addEventListener("click", function () {
-    window.location.reload();
+    const mainPage = document.querySelectorAll(".little-container");
+    mainPage.forEach((x) => x.remove());
+    getNasaApi();
   });
 }
 getNasaApi();
